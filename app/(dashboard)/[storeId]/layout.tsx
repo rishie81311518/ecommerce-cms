1c1842f7e8d42512cb460b6740cc7b649d039bea
Rishi <rishiekandala@gmail.com>
2024-10-10 01:49:41 +0530

    navbar setup

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -30,7 +31,8 @@ export default async function DashboardLayout ({
     return (
         <>
         <div>
-            This will be navbar {children}
+            <Navbar/>
+            {children}
         </div>
         </>
     )
